0a8f1b55e97967c6f670b3fb366a3088a296ff6a
kristieg <91104705+kristieg@users.noreply.github.com>
2021-12-04 17:22:24 -0500

    Update Base.js

--- Base.js
+++ Base.js
@@ -13,9 +13,9 @@ const wordInput = document.querySelector('#input');
 const currentWord = document.querySelector('#currentsentence');
 const scoreDisplay = document.querySelector('#scoredisplay');
 const timeDisplay = document.querySelector('#timedisplay'); //<--we need to make an HTML source for this
-// const startGameButton = document.querySelector('#button') //<--we're gonna need to make a HTML source for this
-// const message = document.querySelector('#message');
-// const seconds = document.querySelector('seconds');
+const startGameButton = document.querySelector('#button') //<--we're gonna need to make a HTML source for this
+const message = document.querySelector('#message');
+const seconds = document.querySelector('seconds');
 
 // array of words to use
 const words = [ //CHANGE ALL words TO SENTENCES LATER WHEN WE EDIT 
