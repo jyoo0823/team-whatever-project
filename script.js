4937f3774d81893399f6e757405f37195ede56bb
vkang6378 <91505648+vkang6378@users.noreply.github.com>
2021-12-05 19:45:09 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,54 @@
+
+
+const startGameBtn = document.getElementById("start-game");
+let inputbox = document.getElementById("inputbox");
+inputbox.classList.add("hidden");
+
+let countdowntime = 6;
+let timertime = -6;
+
+const countdownDisplay = document.querySelector('#countdown');
+let timerDisplay = document.getElementById("countup");
+timerDisplay.classList.add("hidden");
+let timerwordDisplay = document.getElementById("countupword");
+timerwordDisplay.classList.add("hidden");
+
+
+function startGame(){
+  startGameBtn.classList.add("hidden");
+
+   setInterval(countdown, 1000);
+   setInterval(timer, 1000);
+
+}
+
+function functionToDisappearInnerHTML() {
+    countdownDisplay.innerHTML = ""; 
+}
+
+function countdown() {
+  // Make sure time is not run out
+  if (countdowntime > 0) {
+    // Decrement
+    countdowntime--;
+    countdownDisplay.innerHTML = countdowntime;
+  setTimeout(functionToDisappearInnerHTML, 5000);
+  } else if (countdowntime === 0) {
+    // Game is over
+  inputbox.classList.remove("hidden");
+  return 0;
+  }
+  // Show time
+}
+function timer(){
+
+if (timertime >=0){
+  inputbox.classList.remove("hidden");
+  timerDisplay.classList.remove("hidden");
+
+}
+
+    timertime++;
+    timerDisplay.innerHTML = timertime;
+  
+}
